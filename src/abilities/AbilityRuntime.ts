24995fd45a58200fdc3d3a527934193390e46e63
westkitty <andrew.dolby@westcat.ca>
2026-08-13 14:12:30 -0400

    Add ability runtime compatibility barrel

---
+++ src/abilities/AbilityRuntime.ts
@@ -0,0 +1,4 @@
+/** Compatibility barrel for the ability runtime. */
+export { AbilityInstance } from './AbilityInstance';
+export type { AbilityInstanceOptions, AbilityPreviewState } from './AbilityInstance';
+export { AbilityManager } from './AbilityManager';
