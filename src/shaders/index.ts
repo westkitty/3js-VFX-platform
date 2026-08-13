36be742515012e69badcc05b1f17e7cfe40b1ef8
westkitty <andrew.dolby@westcat.ca>
2026-08-13 14:05:58 -0400

    Add shader compatibility barrel

---
+++ src/shaders/index.ts
@@ -0,0 +1,7 @@
+export { TrailMaterialShader, TrailMaterialShader as RibbonShader } from './TrailMaterialShader';
+export { BeamShader } from './BeamShader';
+export { PulseRingShader, PulseRingShader as ShockRingShader } from './PulseRingShader';
+export { SurfaceMarkShader, SurfaceMarkShader as SurfaceDecalShader } from './SurfaceMarkShader';
+export { VisualMoteShader, VisualMoteShader as ParticleInstancedShader } from './VisualMoteShader';
+export { OrbMaterialShader, OrbMaterialShader as VolumetricOrbShader } from './OrbMaterialShader';
+export { BaseSurfaceShader, BaseSurfaceShader as TerrainShader } from './BaseSurfaceShader';
